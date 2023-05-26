acb4701158867843fe7f030fcfbadd9085dc7a09
Daniel Kocot <blusiae@gmail.com>
2023-05-26 13:36:21 +0200

    Add AuthorViewModel

---
+++ BooksLife.Web/Models/AuthorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BooksLife.Web
+{
+    public class AuthorViewModel
+    {
+        public int Id { get; set; }
+        public string Firstname { get; set; } = string.Empty;
+        public string Lastname { get; set; } = string.Empty;
+    }
+}
