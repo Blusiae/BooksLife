7846090fa5389f2cee64fdcaa96d33f4d320b8ee
Daniel Kocot <blusiae@gmail.com>
2023-05-26 13:26:36 +0200

    Add Author DTO

---
+++ BooksLife.Core/Dtos/AuthorDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BooksLife.Core
+{
+    public class AuthorDto
+    {
+        public int Id { get; set; }
+        public string Firstname { get; set; } = string.Empty;
+        [Required]
+        public string Lastname { get; set; }
+    }
+}
