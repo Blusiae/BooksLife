32f2430d4cccab9661b9edab20be646316348b15
Daniel Kocot <blusiae@gmail.com>
2023-05-26 13:32:43 +0200

    Add auto mapper for AuthorEntity and AuthorDto

---
+++ BooksLife.Core/Mappers/DtoMapper.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+
+namespace BooksLife.Core
+{
+    public class DtoMapper
+    {
+        private IMapper _mapper;
+
+        public DtoMapper()
+        {
+            _mapper = new MapperConfiguration(config =>
+            {
+                config.CreateMap<AuthorEntity, AuthorDto>().ReverseMap();
+            }).CreateMapper();
+        }
+
+        public AuthorDto Map(AuthorEntity Author) => _mapper.Map<AuthorDto>(Author); //AuthorEntity -> AuthorDto
+        public IEnumerable<AuthorDto> Map(IEnumerable<AuthorEntity> Authors) => _mapper.Map<IEnumerable<AuthorDto>>(Authors); //Collection of AuthorEntity -> collection of AuthorDto
+
+        public AuthorEntity Map(AuthorDto Author) => _mapper.Map<AuthorEntity>(Author); //AuthorDto -> AuthorEntity
+        public IEnumerable<AuthorEntity> Map(IEnumerable<AuthorDto> Authors) => _mapper.Map<IEnumerable<AuthorEntity>>(Authors); //Collection of AuthorDto -> collection of AuthorEntity
+    }
+}
