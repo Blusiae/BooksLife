4085ced8500fec586d367706a167682f5ca2cd91
Daniel Kocot <blusiae@gmail.com>
2023-05-26 13:58:37 +0200

    Add interface for AuthorManager

---
+++ BooksLife.Core/Interfaces/Managers/IAuthorManager.cs
@@ -0,0 +1,10 @@
+﻿namespace BooksLife.Core
+{
+    public interface IAuthorManager
+    {
+        Response Add(AuthorDto author);
+        Response Delete(int id);
+        AuthorDto Get(int id);
+        List<AuthorDto> GetAll();
+    }
+}
