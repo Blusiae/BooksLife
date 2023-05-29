270ffbf46c334de278f1dfae3dc39fb765069259
Daniel Kocot <blusiae@gmail.com>
2023-05-29 13:33:09 +0200

    Add data annotations to AuthorViewModel

--- BooksLife.Web/Models/AuthorViewModel.cs
+++ BooksLife.Web/Models/AuthorViewModel.cs
@@ -1,9 +1,12 @@
-﻿namespace BooksLife.Web
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BooksLife.Web
 {
     public class AuthorViewModel
     {
         public int Id { get; set; }
         public string Firstname { get; set; } = string.Empty;
+        [Required(AllowEmptyStrings = false)]
         public string Lastname { get; set; } = string.Empty;
     }
 }
