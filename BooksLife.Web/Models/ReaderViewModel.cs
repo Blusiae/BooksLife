2bcd7c4cafdc0e93262a478be6a84be6c33483aa
Daniel Kocot <blusiae@gmail.com>
2023-05-30 17:01:27 +0200

    Add ReaderViewModel

---
+++ BooksLife.Web/Models/ReaderViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace BooksLife.Web
+{
+    public class ReaderViewModel
+    {
+        public Guid Id { get; set; }
+        public string Firstname { get; set; } = string.Empty;
+        public string Lastname { get; set; } = string.Empty;
+        public DateTime Birthdate { get; set; }
+        public string EmailAddress { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+        public AddressViewModel Address { get; set; }
+    }
+}
