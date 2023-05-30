7aeee29a874d0306f469e1e04aa130dbe35d001e
Daniel Kocot <blusiae@gmail.com>
2023-05-30 14:02:06 +0200

    Add Auto Mapper for ReaderDto and AddressDto

--- BooksLife.Core/Mappers/DtoMapper.cs
+++ BooksLife.Core/Mappers/DtoMapper.cs
@@ -11,6 +11,8 @@ namespace BooksLife.Core
             _mapper = new MapperConfiguration(config =>
             {
                 config.CreateMap<AuthorEntity, AuthorDto>().ReverseMap();
+                config.CreateMap<ReaderEntity, ReaderDto>().ReverseMap();
+                config.CreateMap<AddressEntity, AddressDto>().ReverseMap();
             }).CreateMapper();
         }
 
@@ -19,5 +21,14 @@ namespace BooksLife.Core
 
         public AuthorEntity Map(AuthorDto Author) => _mapper.Map<AuthorEntity>(Author); //AuthorDto -> AuthorEntity
         public IEnumerable<AuthorEntity> Map(IEnumerable<AuthorDto> Authors) => _mapper.Map<IEnumerable<AuthorEntity>>(Authors); //Collection of AuthorDto -> collection of AuthorEntity
+
+        public ReaderDto Map(ReaderEntity Reader) => _mapper.Map<ReaderDto>(Reader); //ReaderEntity -> ReaderDto
+        public IEnumerable<ReaderDto> Map(IEnumerable<ReaderEntity> Readers) => _mapper.Map<IEnumerable<ReaderDto>>(Readers); //Colection of ReaderEntity -> collection of ReaderDto
+
+        public ReaderEntity Map(ReaderDto Reader) => _mapper.Map<ReaderEntity>(Reader); //ReaderDto -> ReaderEntity
+        public IEnumerable<ReaderEntity> Map(IEnumerable<ReaderDto> Readers) => _mapper.Map<IEnumerable<ReaderEntity>>(Readers); //Collection of ReaderDto -> collection of ReaderEntity
+
+        public AddressDto Map(AddressEntity Address) => _mapper.Map<AddressDto>(Address); //AddressEntity -> AddressDto
+        public AddressEntity Map(AddressDto Address) => _mapper.Map<AddressEntity>(Address); //AddressDto -> AddressEntity
     }
 }
