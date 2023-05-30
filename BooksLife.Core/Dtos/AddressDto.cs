4f4fe0a1f61fb8aed0f6a870efee492839b86c28
Daniel Kocot <blusiae@gmail.com>
2023-05-30 13:57:12 +0200

    Add Reader and Address Dto

---
+++ BooksLife.Core/Dtos/AddressDto.cs
@@ -0,0 +1,13 @@
+﻿namespace BooksLife.Core
+{
+    public class AddressDto
+    {
+        public Guid Id { get; set; }
+        public string Country { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public string PostalCode { get; set; } = string.Empty;
+        public string Street { get; set; } = string.Empty;
+        public string HouseNumber { get; set; } = string.Empty;
+        public string FlatNumber { get; set; } = string.Empty;
+    }
+}
---
+++ BooksLife.Core/Dtos/ReaderDto.cs
@@ -0,0 +1,13 @@
+﻿namespace BooksLife.Core
+{
+    public class ReaderDto
+    {
+        public Guid Id { get; set; }
+        public string Firstname { get; set; } = string.Empty;
+        public string Lastname { get; set; } = string.Empty;
+        public DateTime Birthdate { get; set; }
+        public string EmailAddress { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+        public AddressDto Address { get; set; }
+    }
+}
