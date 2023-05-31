705d0ca6e72b2787ab5fa7915ff0954883b5ed50
Daniel Kocot <blusiae@gmail.com>
2023-05-31 09:55:09 +0200

    Make not required properties nullable in ReaderDto

--- BooksLife.Core/Dtos/ReaderDto.cs
+++ BooksLife.Core/Dtos/ReaderDto.cs
@@ -6,13 +6,13 @@
         public string Firstname { get; set; } = string.Empty;
         public string Lastname { get; set; } = string.Empty;
         public DateTime Birthdate { get; set; }
-        public string EmailAddress { get; set; } = string.Empty;
-        public string PhoneNumber { get; set; } = string.Empty;
+        public string? EmailAddress { get; set; } = string.Empty;
+        public string? PhoneNumber { get; set; } = string.Empty;
         public string Country { get; set; } = string.Empty;
         public string City { get; set; } = string.Empty;
         public string PostalCode { get; set; } = string.Empty;
         public string Street { get; set; } = string.Empty;
         public string HouseNumber { get; set; } = string.Empty;
-        public string FlatNumber { get; set; } = string.Empty;
+        public string? FlatNumber { get; set; } = string.Empty;
     }
 }
