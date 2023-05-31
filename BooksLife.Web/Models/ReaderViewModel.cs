ec8a9f910008b88b519c479e0c543ed6aa5256e1
Daniel Kocot <blusiae@gmail.com>
2023-05-31 08:43:03 +0200

    Delete AddressViewModel

--- BooksLife.Web/Models/ReaderViewModel.cs
+++ BooksLife.Web/Models/ReaderViewModel.cs
@@ -8,6 +8,11 @@
         public DateTime Birthdate { get; set; }
         public string EmailAddress { get; set; } = string.Empty;
         public string PhoneNumber { get; set; } = string.Empty;
-        public AddressViewModel Address { get; set; }
+        public string Country { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public string PostalCode { get; set; } = string.Empty;
+        public string Street { get; set; } = string.Empty;
+        public string HouseNumber { get; set; } = string.Empty;
+        public string FlatNumber { get; set; } = string.Empty;
     }
 }
