a95bcccfdbac8496b4dc426c45b0d55fe6adbec3
Daniel Kocot <blusiae@gmail.com>
2023-05-31 09:03:29 +0200

    Add ReaderViewModel validation tags

--- BooksLife.Web/Models/ReaderViewModel.cs
+++ BooksLife.Web/Models/ReaderViewModel.cs
@@ -1,16 +1,26 @@
-﻿namespace BooksLife.Web
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BooksLife.Web
 {
     public class ReaderViewModel
     {
         public Guid Id { get; set; }
+        [Required]
         public string Firstname { get; set; } = string.Empty;
+        [Required]
         public string Lastname { get; set; } = string.Empty;
+        [Required]
         public DateTime Birthdate { get; set; }
+        [EmailAddress] 
         public string EmailAddress { get; set; } = string.Empty;
+        [Phone]
         public string PhoneNumber { get; set; } = string.Empty;
+        [Required]
         public string Country { get; set; } = string.Empty;
+        [Required]
         public string City { get; set; } = string.Empty;
         public string PostalCode { get; set; } = string.Empty;
+        [Required]
         public string Street { get; set; } = string.Empty;
         public string HouseNumber { get; set; } = string.Empty;
         public string FlatNumber { get; set; } = string.Empty;
