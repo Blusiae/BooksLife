c1d27acadf1d9251e9f2071045acd1191e7868ba
Daniel Kocot <blusiae@gmail.com>
2023-05-31 18:35:16 +0200

    Add Book repository

---
+++ BooksLife.Database/Repositories/BookRepository.cs
@@ -0,0 +1,12 @@
+﻿using BooksLife.Core;
+using Microsoft.EntityFrameworkCore;
+
+namespace BooksLife.Database
+{
+    public class BookRepository : BaseRepository<BookEntity>, IBookRepository
+    {
+        protected override DbSet<BookEntity> DbSet => _context.Books;
+
+        public BookRepository(ApplicationDbContext context) : base(context) { }
+    }
+}
