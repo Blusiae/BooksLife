391e7cbf87d9c73a210296b4f542182ca1a820cd
Daniel Kocot <blusiae@gmail.com>
2023-05-31 09:31:18 +0200

    Disallow empty strings in validation

--- BooksLife.Web/Models/ReaderViewModel.cs
+++ BooksLife.Web/Models/ReaderViewModel.cs
@@ -5,9 +5,9 @@ namespace BooksLife.Web
     public class ReaderViewModel
     {
         public Guid Id { get; set; }
-        [Required]
+        [Required(AllowEmptyStrings = false)]
         public string Firstname { get; set; } = string.Empty;
-        [Required]
+        [Required(AllowEmptyStrings = false)]
         public string Lastname { get; set; } = string.Empty;
         [Required]
         public DateTime Birthdate { get; set; }
@@ -15,12 +15,12 @@ namespace BooksLife.Web
         public string EmailAddress { get; set; } = string.Empty;
         [Phone]
         public string PhoneNumber { get; set; } = string.Empty;
-        [Required]
+        [Required(AllowEmptyStrings = false)]
         public string Country { get; set; } = string.Empty;
-        [Required]
+        [Required(AllowEmptyStrings = false)]
         public string City { get; set; } = string.Empty;
         public string PostalCode { get; set; } = string.Empty;
-        [Required]
+        [Required(AllowEmptyStrings = false)]
         public string Street { get; set; } = string.Empty;
         public string HouseNumber { get; set; } = string.Empty;
         public string FlatNumber { get; set; } = string.Empty;
