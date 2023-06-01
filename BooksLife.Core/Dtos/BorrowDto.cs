a646460ecabb1fb6b1636862a85e3328c08767ad
Daniel Kocot <blusiae@gmail.com>
2023-06-01 19:29:15 +0200

    Add DtoMapper tests for Borrows

--- BooksLife.Core/Dtos/BorrowDto.cs
+++ BooksLife.Core/Dtos/BorrowDto.cs
@@ -4,9 +4,9 @@
     {
         public Guid Id { get; set; }
         public Guid BookId { get; set; }
-        public BookEntity Book { get; set; } = new BookEntity();
+        public BookDto Book { get; set; } = new BookDto();
         public Guid ReaderId { get; set; }
-        public ReaderEntity Reader { get; set; } = new ReaderEntity();
+        public ReaderDto Reader { get; set; } = new ReaderDto();
         public DateTime BorrowDate { get; set; }
         public DateTime ReturnDate { get; set; }
     }
