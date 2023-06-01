9b3c4033a6b5148525949346a115e877dccc353c
Daniel Kocot <blusiae@gmail.com>
2023-06-01 19:31:15 +0200

    Add IBorrowManager interface

---
+++ BooksLife.Core/Interfaces/Managers/IBorrowManager.cs
@@ -0,0 +1,10 @@
+﻿namespace BooksLife.Core
+{
+    public interface IBorrowManager
+    {
+        Response Add(BorrowDto borrowDto);
+        Response Remove(Guid id);
+        List<BorrowDto> GetAll();
+        BorrowDto Get(Guid id);
+    }
+}
