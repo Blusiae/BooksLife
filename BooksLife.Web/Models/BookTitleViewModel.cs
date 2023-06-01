1cfab82332a005ff91a73e50c17cae4c232974d4
Daniel Kocot <blusiae@gmail.com>
2023-06-01 14:18:12 +0200

    Add BookTitleViewModel

--- BooksLife.Web/Models/BookTitleViewModel.cs
+++ BooksLife.Web/Models/BookTitleViewModel.cs
@@ -1,11 +1,16 @@
-﻿namespace BooksLife.Core
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BooksLife.Web
 {
-    public class BookTitleDto
+    public class BookTitleViewModel
     {
         public Guid Id { get; set; }
+        [Required]
         public string Title { get; set; } = string.Empty;
+        [Required]
         public int PublicationYear { get; set; }
         public string? AuthorName { get; set; } = string.Empty;
+        [Required]
         public Guid AuthorId { get; set; }
     }
 }
