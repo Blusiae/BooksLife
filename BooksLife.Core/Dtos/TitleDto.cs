cf34cca85a724bb591400ba0bb168c096380b24d
Daniel Kocot <blusiae@gmail.com>
2023-06-01 11:47:44 +0200

    Add TitleDto

---
+++ BooksLife.Core/Dtos/TitleDto.cs
@@ -0,0 +1,11 @@
+﻿namespace BooksLife.Core
+{
+    public class TitleDto
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public int PublicationYear { get; set; }
+        public AuthorDto Author { get; set; }
+        public Guid AuthorId { get; set; }
+    }
+}
