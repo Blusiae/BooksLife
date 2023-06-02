a303a7a9fcf2eba8c15bc5ec177f6f6455a6f7de
Daniel Kocot <blusiae@gmail.com>
2023-06-02 10:27:39 +0200

    Add BorrowManager

---
+++ BooksLife.Core/Managers/BorrowManager.cs
@@ -0,0 +1,64 @@
+﻿namespace BooksLife.Core
+{
+    public class BorrowManager : IBorrowManager
+    {
+        private readonly IBorrowRepository _borrowRepository;
+        private readonly IDtoMapper _mapper;
+
+        private const string FAILED_MESSAGE = "Something went wrong!";
+        private const string SUCCEED_ADD_MESSAGE = "A new borrow has been added.";
+        private const string SUCCEED_REMOVE_MESSAGE = "Borrow has been removed.";
+
+        public BorrowManager(IBorrowRepository borrowRepository, IDtoMapper mapper)
+        {
+            _borrowRepository = borrowRepository;
+            _mapper = mapper;
+        }
+
+        public Response Add(BorrowDto borrowDto)
+        {
+            var borrowEntity = _mapper.Map(borrowDto);
+            if (_borrowRepository.Add(borrowEntity))
+            {
+                return new Response()
+                {
+                    Succeed = true,
+                    Message = SUCCEED_ADD_MESSAGE
+                };
+            }
+            return new Response()
+            {
+                Succeed = false,
+                Message = FAILED_MESSAGE
+            };
+        }
+
+        public BorrowDto Get(Guid id)
+        {
+            return _mapper.Map(_borrowRepository.Get(id));
+        }
+
+        public List<BorrowDto> GetAll()
+        {
+            return _mapper.Map(_borrowRepository.GetAll()).ToList();
+        }
+
+        public Response Remove(Guid id)
+        {
+            if (_borrowRepository.Remove(id))
+            {
+                return new Response()
+                {
+                    Succeed = true,
+                    Message = SUCCEED_REMOVE_MESSAGE
+                };
+            }
+
+            return new Response()
+            {
+                Succeed = false,
+                Message = FAILED_MESSAGE
+            };
+        }
+    }
+}
