77a3d0560dfbf554e4913d15bf2113e224908b57
Daniel Kocot <blusiae@gmail.com>
2023-06-02 12:01:07 +0200

    Add SetAsReturned method tests

--- BooksLife.Tests/Core/Managers/BorrowManagerTests.cs
+++ BooksLife.Tests/Core/Managers/BorrowManagerTests.cs
@@ -10,6 +10,7 @@ namespace BooksLife.Tests
         private const string FAILED_MESSAGE = "Something went wrong!";
         private const string SUCCEED_ADD_MESSAGE = "A new borrow has been added.";
         private const string SUCCEED_REMOVE_MESSAGE = "Borrow has been removed.";
+        private const string SUCCED_RETURNED_MESSAGE = "Marked borrow as returned.";
         [Theory]
         [InlineData(true, SUCCEED_ADD_MESSAGE)]
         [InlineData(false, FAILED_MESSAGE)]
@@ -38,7 +39,7 @@ namespace BooksLife.Tests
             borrowRepositoryMock.Setup(m => m.Remove(It.IsAny<Guid>())).Returns(dbResponse);
             var dtoMapperMock = new Mock<IDtoMapper>();
             var bookRepositoryMock = new Mock<IBookRepository>();
-            bookRepositoryMock.Setup(m => m.SetAsUnborrowed(It.IsAny<Guid>())).Returns(true);
+            //bookRepositoryMock.Setup(m => m.SetAsUnborrowed(It.IsAny<Guid>())).Returns(true);
             var borrowManager = new BorrowManager(borrowRepositoryMock.Object, dtoMapperMock.Object, bookRepositoryMock.Object);
 
             var result = borrowManager.Remove(Guid.NewGuid());
@@ -46,5 +47,26 @@ namespace BooksLife.Tests
             result.Succeed.Should().Be(dbResponse);
             result.Message.Should().Be(message);
         }
+
+        [Theory]
+        [InlineData(true, true, SUCCED_RETURNED_MESSAGE)]
+        [InlineData(false, false, FAILED_MESSAGE)]
+        [InlineData(true, false, FAILED_MESSAGE)]
+        [InlineData(false, true, FAILED_MESSAGE)]
+        public void SetAsReturned_ForResponseFromDatabase_ShouldReturnResponseObjectWithCorrectMessage(bool borrowResponse, bool bookResponse, string message)
+        {
+            var borrowRepositoryMock = new Mock<IBorrowRepository>();
+            borrowRepositoryMock.Setup(m => m.SetAsUnactive(It.IsAny<Guid>())).Returns(borrowResponse);
+            var dtoMapperMock = new Mock<IDtoMapper>();
+            var bookRepositoryMock = new Mock<IBookRepository>();
+            bookRepositoryMock.Setup(m => m.SetAsUnborrowed(It.IsAny<Guid>())).Returns(bookResponse);
+            var borrowManager = new BorrowManager(borrowRepositoryMock.Object, dtoMapperMock.Object, bookRepositoryMock.Object);
+
+            var result = borrowManager.SetAsReturned(new BorrowDto() { Id = Guid.NewGuid(), BookId = Guid.NewGuid()});
+
+            result.Succeed.Should().Be(borrowResponse && bookResponse);
+            result.Message.Should().Be(message);
+        }
+
     }
 }
