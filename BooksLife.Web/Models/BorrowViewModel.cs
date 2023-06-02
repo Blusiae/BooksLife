615703d0a5382b653b36f5261fa3bb006083ef55
Daniel Kocot <blusiae@gmail.com>
2023-06-02 10:42:11 +0200

    Add BorrowViewModel

---
+++ BooksLife.Web/Models/BorrowViewModel.cs
@@ -0,0 +1,21 @@
+﻿using BooksLife.Core;
+using System.ComponentModel.DataAnnotations;
+
+namespace BooksLife.Web
+{
+    public class BorrowViewModel
+    {
+        public Guid Id { get; set; }
+        public bool IsActive { get; set; } = true;
+        [Required]
+        public Guid BookId { get; set; }
+        public BookViewModel Book { get; set; } = new BookViewModel();
+        [Required]
+        public Guid ReaderId { get; set; }
+        public ReaderViewModel Reader { get; set; } = new ReaderViewModel();
+        [Required]
+        public DateTime BorrowDate { get; set; }
+        [Required]
+        public DateTime ReturnDate { get; set; }
+    }
+}
