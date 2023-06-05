35b7511f661687bb89b4d7382689e30f97b72b63
Daniel Kocot <blusiae@gmail.com>
2023-06-05 12:45:36 +0200

    Add Dto mappers for Reader

--- BooksLife.Core/Mappers/DtoMapExtensions.cs
+++ BooksLife.Core/Mappers/DtoMapExtensions.cs
@@ -16,5 +16,29 @@
         {
             return authorEntities.Select(x => x.ToDto()).ToList();
         }
+
+        public static ReaderDto ToDto(this ReaderEntity readerEntity)
+        {
+            return new ReaderDto()
+            {
+                Id = readerEntity.Id,
+                Firstname = readerEntity.Firstname,
+                Lastname = readerEntity.Lastname,
+                Birthdate = readerEntity.Birthdate,
+                EmailAddress = readerEntity.EmailAddress,
+                PhoneNumber = readerEntity.PhoneNumber,
+                Country = readerEntity.Address.Country,
+                City = readerEntity.Address.City,
+                PostalCode = readerEntity.Address.PostalCode,
+                Street = readerEntity.Address.Street,
+                HouseNumber = readerEntity.Address.HouseNumber,
+                FlatNumber = readerEntity.Address.FlatNumber
+            };
+        }
+
+        public static List<ReaderDto> ToDto(this IEnumerable<ReaderEntity> readerEntities)
+        {
+            return readerEntities.Select(x => x.ToDto()).ToList();
+        }
     }
 }
