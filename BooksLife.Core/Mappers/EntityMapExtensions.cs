9bf61d884346e3f62814e762c65b69125a641690
Daniel Kocot <blusiae@gmail.com>
2023-06-05 12:24:32 +0200

    Add EntityMapper for Authors

---
+++ BooksLife.Core/Mappers/EntityMapExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace BooksLife.Core
+{
+    public static class EntityMapExtensions
+    {
+        public static AuthorEntity ToEntity(this AuthorDto authorDto)
+        {
+            return new AuthorEntity()
+            {
+                Id = authorDto.Id,
+                Firstname = authorDto.Firstname,
+                Lastname = authorDto.Lastname
+            };
+        }
+
+        public static List<AuthorEntity> ToEntity(this IEnumerable<AuthorDto> authorDtos)
+        {
+            return authorDtos.Select(x => ToEntity(x)).ToList();
+        }
+    }
+}
