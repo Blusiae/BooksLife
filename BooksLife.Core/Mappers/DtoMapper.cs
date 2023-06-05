86e29de1b7b3a129ea519279f92f438dbcf45ca6
Daniel Kocot <blusiae@gmail.com>
2023-06-05 12:08:53 +0200

    Add mappers for Authors

--- BooksLife.Core/Mappers/DtoMapper.cs
+++ BooksLife.Core/Mappers/DtoMapper.cs
@@ -1,65 +1,20 @@
-﻿using AutoMapper;
-
-namespace BooksLife.Core
+﻿namespace BooksLife.Core
 {
-    public class DtoMapper : IDtoMapper
+    public static class DtoMapExtensions
     {
-        private IMapper _mapper;
-
-        public DtoMapper()
+       public static AuthorDto ToDto(this AuthorEntity authorEntity)
         {
-            _mapper = new MapperConfiguration(config =>
+            return new AuthorDto()
             {
-                config.CreateMap<AuthorEntity, AuthorDto>().ReverseMap();
-                config.CreateMap<ReaderEntity, ReaderDto>()
-                    .ForMember(x => x.Country, y => y.MapFrom(z => z.Address.Country))
-                    .ForMember(x => x.City, y => y.MapFrom(z => z.Address.City))
-                    .ForMember(x => x.PostalCode, y => y.MapFrom(z => z.Address.PostalCode))
-                    .ForMember(x => x.Street, y => y.MapFrom(z => z.Address.Street))
-                    .ForMember(x => x.HouseNumber, y => y.MapFrom(z => z.Address.HouseNumber))
-                    .ForMember(x => x.FlatNumber, y => y.MapFrom(z => z.Address.FlatNumber))
-                    .ReverseMap();
-                config.CreateMap<BookDto, BookEntity>();
-                config.CreateMap<BookEntity, BookDto>()
-                    .ForMember(x => x.AuthorName, y => y.MapFrom(z => $"{z.BookTitle.Author.Firstname} {z.BookTitle.Author.Lastname}"))
-                    .ForMember(x => x.Title, y => y.MapFrom(z => z.BookTitle.Title))
-                    .ForMember(x => x.PublicationYear, y => y.MapFrom(z => z.BookTitle.PublicationYear));
-                config.CreateMap<BookTitleDto, BookTitleEntity>();
-                config.CreateMap<BookTitleEntity, BookTitleDto>()
-                    .ForMember(x => x.AuthorName, y => y.MapFrom(z => $"{z.Author.Firstname} {z.Author.Lastname}"));
-                config.CreateMap<BorrowEntity, BorrowDto>().ReverseMap();
-            }).CreateMapper();
+                Id = authorEntity.Id,
+                Firstname = authorEntity.Firstname,
+                Lastname = authorEntity.Lastname
+            };
         }
 
-        public AuthorDto Map(AuthorEntity Author) => _mapper.Map<AuthorDto>(Author); //AuthorEntity -> AuthorDto
-        public IEnumerable<AuthorDto> Map(IEnumerable<AuthorEntity> Authors) => _mapper.Map<IEnumerable<AuthorDto>>(Authors); //Collection of AuthorEntity -> collection of AuthorDto
-
-        public AuthorEntity Map(AuthorDto Author) => _mapper.Map<AuthorEntity>(Author); //AuthorDto -> AuthorEntity
-        public IEnumerable<AuthorEntity> Map(IEnumerable<AuthorDto> Authors) => _mapper.Map<IEnumerable<AuthorEntity>>(Authors); //Collection of AuthorDto -> collection of AuthorEntity
-
-        public ReaderDto Map(ReaderEntity Reader) => _mapper.Map<ReaderDto>(Reader); //ReaderEntity -> ReaderDto
-        public IEnumerable<ReaderDto> Map(IEnumerable<ReaderEntity> Readers) => _mapper.Map<IEnumerable<ReaderDto>>(Readers); //Colection of ReaderEntity -> collection of ReaderDto
-
-        public ReaderEntity Map(ReaderDto Reader) => _mapper.Map<ReaderEntity>(Reader); //ReaderDto -> ReaderEntity
-        public IEnumerable<ReaderEntity> Map(IEnumerable<ReaderDto> Readers) => _mapper.Map<IEnumerable<ReaderEntity>>(Readers); //Collection of ReaderDto -> collection of ReaderEntity
-
-        public BookDto Map(BookEntity Book) => _mapper.Map<BookDto>(Book); //BookEntity -> BookDto
-        public IEnumerable<BookDto> Map(IEnumerable<BookEntity> Books) => _mapper.Map<IEnumerable<BookDto>>(Books); //Collection of BookEntity -> collection of BookDto
-
-        public BookEntity Map(BookDto Book) => _mapper.Map<BookEntity>(Book); //BookDto -> BookEntity
-        public IEnumerable<BookEntity> Map(IEnumerable<BookDto> Books) => _mapper.Map<IEnumerable<BookEntity>>(Books); //Collection of BookDto -> collection of BookEntity
-
-        public BookTitleDto Map(BookTitleEntity BookTitle) => _mapper.Map<BookTitleDto>(BookTitle); //BookTitleEntity -> BookTitleDto
-        public IEnumerable<BookTitleDto> Map(IEnumerable<BookTitleEntity> BookTitles) => _mapper.Map<IEnumerable<BookTitleDto>>(BookTitles); //Collection of BookTitleEntity -> collection of BookTitleDto
-
-        public BookTitleEntity Map(BookTitleDto BookTitle) => _mapper.Map<BookTitleEntity>(BookTitle); //BookTitleDto -> BookTitleEntity
-        public IEnumerable<BookTitleEntity> Map(IEnumerable<BookTitleDto> BookTitles) => _mapper.Map<IEnumerable<BookTitleEntity>>(BookTitles); //Collection of BookTitleDto -> collection of BookTitleEntity
-        
-        public BorrowDto Map(BorrowEntity Borrow) => _mapper.Map<BorrowDto>(Borrow); //BorrowEntity -> BorrowDto
-        public IEnumerable<BorrowDto> Map(IEnumerable<BorrowEntity> Borrows) => _mapper.Map<IEnumerable<BorrowDto>>(Borrows); //Collection of BorrowEntity -> collection of BorrowDto
-
-        public BorrowEntity Map(BorrowDto Borrow) => _mapper.Map<BorrowEntity>(Borrow); //BorrowDto -> BorrowEntity
-        public IEnumerable<BorrowEntity> Map(IEnumerable<BorrowDto> Borrows) => _mapper.Map<IEnumerable<BorrowEntity>>(Borrows); //Collection of BorrowDto -> collection of BorrowEntity
-
+        public static List<AuthorDto> ToDto(this IEnumerable<AuthorEntity> authorEntities)
+        {
+            return authorEntities.Select(x => x.ToDto()).ToList();
+        }
     }
 }
