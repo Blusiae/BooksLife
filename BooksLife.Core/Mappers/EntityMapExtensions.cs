e31ca88a870ce8c1f23fbd16ce69b9750ccc293e
Daniel Kocot <blusiae@gmail.com>
2023-06-06 09:36:25 +0200

    Add Entity mappers for Readers

--- BooksLife.Core/Mappers/EntityMapExtensions.cs
+++ BooksLife.Core/Mappers/EntityMapExtensions.cs
@@ -14,7 +14,34 @@
 
         public static List<AuthorEntity> ToEntity(this IEnumerable<AuthorDto> authorDtos)
         {
-            return authorDtos.Select(x => ToEntity(x)).ToList();
+            return authorDtos.Select(x => x.ToEntity()).ToList();
+        }
+
+        public static ReaderEntity ToEntity(this ReaderDto readerDto)
+        {
+            return new ReaderEntity()
+            {
+                Id = readerDto.Id,
+                Firstname = readerDto.Firstname,
+                Lastname = readerDto.Lastname,
+                Birthdate = readerDto.Birthdate,
+                EmailAddress = readerDto.EmailAddress,
+                PhoneNumber = readerDto.PhoneNumber,
+                Address = new AddressEntity()
+                {
+                    Country = readerDto.Country,
+                    City = readerDto.City,
+                    PostalCode = readerDto.PostalCode,
+                    Street = readerDto.Street,
+                    HouseNumber = readerDto.HouseNumber,
+                    FlatNumber = readerDto.FlatNumber
+                }
+            };
+        }
+
+        public static List<ReaderEntity> ToEntity(this IEnumerable<ReaderDto> readerDtos)
+        {
+            return readerDtos.Select(x => x.ToEntity()).ToList();
         }
     }
 }
