693a39766b0dfb0f0794b657fdf463cd15ea5fc0
Daniel Kocot <blusiae@gmail.com>
2023-06-14 21:57:17 +0200

    Add Author (entity) mapping

---
+++ BooksLife.Core/Mappers/DtoToEntity.cs
@@ -0,0 +1,16 @@
+﻿namespace BooksLife.Core
+{
+    public static class DtoToEntity
+    {
+        public static AuthorEntity ToEntity(this AuthorDto dto)
+            => new()
+            {
+                Firstname = dto.Firstname,
+                Lastname = dto.Lastname
+            };
+
+        public static List<AuthorEntity> ToEntity(this IEnumerable<AuthorDto> dtos)
+            => dtos.Select(x => x.ToEntity()).ToList();
+
+    }
+}
---
+++ BooksLife.Core/Mappers/EntityToDto.cs
@@ -0,0 +1,17 @@
+﻿namespace BooksLife.Core
+{
+    public static class EntityToDto
+    {
+        public static AuthorDto ToDto(this AuthorEntity entity)
+            => new()
+            {
+                Id = entity.Id,
+                Firstname = entity.Firstname,
+                Lastname = entity.Lastname
+            };
+
+        public static List<AuthorDto> ToDto(this IEnumerable<AuthorEntity> entities)
+            => entities.Select(x => x.ToDto()).ToList();
+
+    }
+}
