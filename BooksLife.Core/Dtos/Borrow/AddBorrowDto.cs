1a2837c7ea70307d593ea7b52d305814206b761e
Daniel Kocot <blusiae@gmail.com>
2023-06-15 12:45:31 +0200

    Create AddBorrowDto

---
+++ BooksLife.Core/Dtos/Borrow/AddBorrowDto.cs
@@ -0,0 +1,11 @@
+﻿namespace BooksLife.Core
+{
+    public class AddBorrowDto
+    {
+        public bool IsActive { get; set; }
+        public Guid BookId { get; set; }
+        public Guid ReaderId { get; set; }
+        public DateTime BorrowDate { get; set; }
+        public DateTime ReturnDate { get; set; }
+    }
+}
