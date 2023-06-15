85a5a20b1902d91ef3d5d7645be3d213e7dbe621
Daniel Kocot <blusiae@gmail.com>
2023-06-15 12:18:25 +0200

    Create AddBookDto

--- BooksLife.Core/Dtos/Book/AddBookDto.cs
+++ BooksLife.Core/Dtos/Book/AddBookDto.cs
@@ -1,13 +1,10 @@
 ﻿namespace BooksLife.Core
 {
-    public class BookDto
+    public class AddBookDto
     {
-        public Guid Id { get; set; }
         public bool IsBorrowed { get; set; }
-        public string Title { get; set; } = string.Empty;
-        public int PublicationYear { get; set; }
+        public Guid BookTitleId { get; set; }
         public int EditionPublicationYear { get; set; }
-        public string AuthorName { get; set; }
         public BookCondition Condition { get; set; }
         public string? ConditionNote { get; set; } = string.Empty;
     }
