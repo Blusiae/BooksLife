da3f781b029caa4f6d5aa3432a5e7d3b6e614f05
Daniel Kocot <blusiae@gmail.com>
2023-06-15 12:30:02 +0200

    Create AddReaderDto.cs

---
+++ BooksLife.Core/Dtos/Reader/AddReaderDto.cs
@@ -0,0 +1,17 @@
+﻿namespace BooksLife.Core
+{
+    public class AddReaderDto
+    {
+        public string Firstname { get; set; } = string.Empty;
+        public string Lastname { get; set; } = string.Empty;
+        public DateTime Birthdate { get; set; }
+        public string? EmailAddress { get; set; } = string.Empty;
+        public string? PhoneNumber { get; set; } = string.Empty;
+        public string Country { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public string PostalCode { get; set; } = string.Empty;
+        public string Street { get; set; } = string.Empty;
+        public string HouseNumber { get; set; } = string.Empty;
+        public string? FlatNumber { get; set; } = string.Empty;
+    }
+}
