3c3d43a200ec2d1ec3e4e3f36005814b7eb1d953
Daniel Kocot <blusiae@gmail.com>
2023-06-29 12:58:42 +0200

    Create AddAuthorViewModel

---
+++ BooksLife.Web/Models/Author/AddAuthorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BooksLife.Web
+{
+    public class AddAuthorViewModel
+    {
+        public string? Firstname { get; set; }
+        [Required(AllowEmptyStrings = false)]
+        public string Lastname { get; set; } = string.Empty;
+    }
+}
--- BooksLife.Web/Models/Author/AuthorViewModel.cs
+++ BooksLife.Web/Models/Author/AuthorViewModel.cs
@@ -6,7 +6,6 @@ namespace BooksLife.Web
     {
         public Guid Id { get; set; }
         public string Firstname { get; set; } = string.Empty;
-        [Required(AllowEmptyStrings = false)]
         public string Lastname { get; set; } = string.Empty;
     }
 }
