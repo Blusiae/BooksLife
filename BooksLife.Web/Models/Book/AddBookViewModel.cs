6953eee7d0d797ccde3d54da2659fddcb6ee8653
Daniel Kocot <blusiae@gmail.com>
2023-06-29 13:30:07 +0200

    Create AddBookViewModel

--- BooksLife.Web/Models/Book/AddBookViewModel.cs
+++ BooksLife.Web/Models/Book/AddBookViewModel.cs
@@ -3,18 +3,12 @@ using System.ComponentModel.DataAnnotations;
 
 namespace BooksLife.Web
 {
-    public class BookViewModel
+    public class AddBookViewModel
     {
-        public bool IsBorrowed { get; set; } = false;
-        public Guid Id { get; set; }
-        public string? Title { get; set; } = string.Empty;
+        public bool IsBorrowed { get; set; }
         [Required]
         public Guid BookTitleId { get; set; }
-        public int PublicationYear { get; set; }
-        [Required]
         public int EditionPublicationYear { get; set; }
-        public string AuthorName { get; set; } = string.Empty;
-        public Guid AuthorId { get; set; }
         [Required]
         public BookCondition Condition { get; set; }
         public string? ConditionNote { get; set; } = string.Empty;
---
+++ BooksLife.Web/Models/Book/BookViewModel.cs
@@ -0,0 +1,16 @@
+﻿using BooksLife.Core;
+
+namespace BooksLife.Web
+{
+    public class BookViewModel
+    {
+        public bool IsBorrowed { get; set; } = false;
+        public Guid Id { get; set; }
+        public string? Title { get; set; } = string.Empty;
+        public int PublicationYear { get; set; }
+        public int EditionPublicationYear { get; set; }
+        public string AuthorName { get; set; } = string.Empty;
+        public BookCondition Condition { get; set; }
+        public string? ConditionNote { get; set; } = string.Empty;
+    }
+}
