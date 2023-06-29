6a9b7ca312fa37631675141c7bf18804baa24930
Daniel Kocot <blusiae@gmail.com>
2023-06-29 13:27:06 +0200

    Add BookTitle ViewModel mapping methods

--- BooksLife.Web/Mappers/ViewModelMapping.cs
+++ BooksLife.Web/Mappers/ViewModelMapping.cs
@@ -21,5 +21,25 @@ namespace BooksLife.Core
 
         public static List<AuthorViewModel> ToViewModel(this IEnumerable<AuthorDto> dtos)
             => dtos.Select(x => x.ToViewModel()).ToList();
+
+        public static BookTitleViewModel ToViewModel(this BookTitleDto dto)
+            => new()
+            {
+                Id = dto.Id,
+                Title = dto.Title,
+                PublicationYear = dto.PublicationYear,
+                AuthorName = dto.AuthorName,
+            };
+
+        public static AddBookTitleDto ToDto(this AddBookTitleViewModel viewModel)
+            => new()
+            {
+                Title = viewModel.Title,
+                PublicationYear = viewModel.PublicationYear,
+                AuthorId = viewModel.AuthorId
+            };
+
+        public static List<BookTitleViewModel> ToViewModel(this IEnumerable<BookTitleDto> dtos)
+            => dtos.Select(x => x.ToViewModel()).ToList();
     }
 }
