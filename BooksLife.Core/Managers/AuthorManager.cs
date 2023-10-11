edbbbb706b608929a47ab1943f55484e2f9f07e1
Daniel Kocot <42910231+Blusiae@users.noreply.github.com>
2023-10-11 12:35:55 +0200

    Use UnitOfWork in AuthorManager

--- BooksLife.Core/Managers/AuthorManager.cs
+++ BooksLife.Core/Managers/AuthorManager.cs
@@ -2,20 +2,20 @@
 {
     public class AuthorManager : IAuthorManager
     {
-        private readonly IAuthorRepository _authorRepository;
+        private readonly IUnitOfWork _unitOfWork;
 
         private const string FAILED_MESSAGE = "Something went wrong!";
         private const string SUCCEED_ADD_MESSAGE = "A new author has been added.";
         private const string SUCCEED_REMOVE_MESSAGE = "Author has been removed.";
 
-        public AuthorManager(IAuthorRepository authorRepository)
+        public AuthorManager(IUnitOfWork unitOfWork)
         {
-            _authorRepository = authorRepository;
+            _unitOfWork = unitOfWork;
         }
         public Response Add(AddAuthorDto author)
         {
             var authorEntity = author.ToEntity();
-            var dbResponse = _authorRepository.Create(authorEntity);
+            var dbResponse = _unitOfWork.AuthorRepository.Create(authorEntity);
             if (dbResponse)
             {
                 return new Response()
@@ -35,11 +35,11 @@
 
         public Response Remove(Guid id)
         {
-            var author = _authorRepository.GetById(id);
+            var author = _unitOfWork.AuthorRepository.GetById(id);
 
             if(author != null)
             {
-                var dbResponse = _authorRepository.Delete(author);
+                var dbResponse = _unitOfWork.AuthorRepository.Delete(author);
 
                 if (dbResponse)
                 {
@@ -60,7 +60,7 @@
 
         public AuthorDto Get(Guid id)
         {
-            return _authorRepository.GetById(id).ToDto();
+            return _unitOfWork.AuthorRepository.GetById(id).ToDto();
         }
 
         public IEnumerable<AuthorDto> GetPage(int pageSize, int pageNumber, string? filterString, out int totalCount)
@@ -68,16 +68,16 @@
             var filteringMethod = new Func<AuthorEntity, bool>(a => string.IsNullOrEmpty(filterString)
             || string.Join(' ', a.Firstname, a.Lastname).ToLower().Contains(filterString.ToLower()));
 
-            totalCount = _authorRepository.Count(filteringMethod);
+            totalCount = _unitOfWork.AuthorRepository.Count(filteringMethod);
 
-            var authors = _authorRepository.GetFilteredPage(filteringMethod, pageSize, (pageNumber - 1) * pageSize);
+            var authors = _unitOfWork.AuthorRepository.GetFilteredPage(filteringMethod, pageSize, (pageNumber - 1) * pageSize);
 
             return authors.ToDto();
         }
 
         public IEnumerable<AuthorDto> GetAll()
         {
-            var authors = _authorRepository.GetAll();
+            var authors = _unitOfWork.AuthorRepository.GetAll();
 
             return authors.ToDto();
         }
