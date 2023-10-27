f7aae30dd814ddb63527ccda27716664b2b5b437
Daniel Kocot <42910231+Blusiae@users.noreply.github.com>
2023-10-27 15:18:20 +0200

    Add configuration for Author

---
+++ BooksLife.Database/Configurations/AuthorConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using BooksLife.Core;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooksLife.Database.Configurations
+{
+    internal class AuthorConfiguration : IEntityTypeConfiguration<AuthorEntity>
+    {
+        public void Configure(EntityTypeBuilder<AuthorEntity> builder)
+        {
+            builder.Property(a => a.Lastname)
+                .IsRequired();
+        }
+    }
+}
