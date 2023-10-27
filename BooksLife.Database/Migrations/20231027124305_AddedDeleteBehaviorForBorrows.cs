85acb3379ff8d29e33edbfc8959f6eef19e1e948
Daniel Kocot <42910231+Blusiae@users.noreply.github.com>
2023-10-27 14:48:33 +0200

    Add configuration for Borrows

---
+++ BooksLife.Database/Migrations/20231027124305_AddedDeleteBehaviorForBorrows.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BooksLife.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedDeleteBehaviorForBorrows : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Borrows_Books_BookId",
+                table: "Borrows");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Borrows_Readers_ReaderId",
+                table: "Borrows");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Borrows_Books_BookId",
+                table: "Borrows",
+                column: "BookId",
+                principalTable: "Books",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Borrows_Readers_ReaderId",
+                table: "Borrows",
+                column: "ReaderId",
+                principalTable: "Readers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Borrows_Books_BookId",
+                table: "Borrows");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Borrows_Readers_ReaderId",
+                table: "Borrows");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Borrows_Books_BookId",
+                table: "Borrows",
+                column: "BookId",
+                principalTable: "Books",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Borrows_Readers_ReaderId",
+                table: "Borrows",
+                column: "ReaderId",
+                principalTable: "Readers",
+                principalColumn: "Id");
+        }
+    }
+}
